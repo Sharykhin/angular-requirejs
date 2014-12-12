4163e1ed4a6de0a7ac5b1dcb634f4338a93e6ae5
Siarhei Sharykhin <siarhei.sharykhin@gmail.com>
2014-12-12 09:51:43 +0300

    add login functionality

--- app/src/backend/controllers/AuthController.js
+++ app/src/backend/controllers/AuthController.js
@@ -6,17 +6,36 @@ define([
 
     var name = moduleConfig.name + '.AuthController';
 
-    var dependencies = ['$scope','$cookieStore','$http'];
+    var dependencies = ['$scope','$cookieStore','$http','authUrl','userUrl'];
 
     var controller = function($scope,$cookieStore,$http,authUrl,userUrl) {
         var uid = $cookieStore.get('uid');
-        if(uid !== undefined) {
-            $http.get(userUrl+uid,{withCredentials : true}).success(function(data){
 
+        $scope.error=null;
+
+
+        if(uid !== undefined) {
+            $http.get(userUrl+uid,{withCredentials : true}).success(function(response){
+                if(response.id == uid) {
+                    console.log('redirect me');
+                }
             });
-        }
-        $cookieStore.remove('token');
-        console.log(token);
+        };
+
+        $scope.authenticate = function() {
+            $scope.error=null;
+            $http.post(authUrl,{username:$scope.username,password:$scope.password})
+                .success(function(response){
+                    $cookieStore.put('uid',response.uid);
+                })
+                .error(function(response){
+                    var status = response.status;
+                    if(status == 401) {
+                        $scope.error = 'Bad credentials';
+                    }
+                });
+        };
+
         $scope.legend = 'Login form';
 
     };
